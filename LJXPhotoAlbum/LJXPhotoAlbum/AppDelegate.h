3089e9809b0f5bd0059917bfc925e8249da964b4
ChordLeafe <jianxin.li@haha.local>
2016-04-14 11:00:55 +0800

    创建项目

---
+++ LJXPhotoAlbum/LJXPhotoAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJXPhotoAlbum
+//
+//  Created by jianxin.li on 16/4/14.
+//  Copyright © 2016年 m6go.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJXPhotoAlbum/LJXPhotoAlbum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LJXPhotoAlbum
+//
+//  Created by jianxin.li on 16/4/14.
+//  Copyright © 2016年 m6go.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
